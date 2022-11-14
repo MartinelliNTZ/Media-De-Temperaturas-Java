375d05ffb834411cd4da118fffa65915b559d1cc
MartinelliNTZ <martinelli.matheus0@gmail.com>
2022-11-14 00:49:33 -0300

    Finish

--- Main.java
+++ Main.java
@@ -1,6 +1,34 @@
-package PACKAGE_NAME;/**
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
 *@author Matheus Martinelli
 *14-11-2022
 */
 public class Main {
+    public static void main(String[] args) {
+        List<Temperatura> temps = new ArrayList<>(){{
+            add(new Temperatura(39, "Janeiro"));
+            add(new Temperatura(40, "Fevereiro"));
+            add(new Temperatura(35, "Março"));
+            add(new Temperatura(21, "Abril"));
+            add(new Temperatura(13, "Maio"));
+            add(new Temperatura(20, "Junho"));
+        }};
+
+        int total = 0;
+        for (Temperatura t:temps             ) {
+            total+= t.getValor();
+        }
+        Integer media = total/temps.size();
+        System.out.println("Média = "+media);
+        System.out.println("\nMeses acima da média");
+
+        for (Temperatura t:temps             ) {
+            if (media <=t.getValor()) {
+                System.out.println("Mês: "+t.getMes()+".\t Temperatura: "+t.getValor());
+            }
+        }
+    }
 }
--- Temperatura.java
+++ Temperatura.java
@@ -1,6 +1,37 @@
-package PACKAGE_NAME;/**
-*@author Matheus Martinelli
-*14-11-2022
-*/
+/**
+ * @author Matheus Martinelli
+ * 14-11-2022
+ */
 public class Temperatura {
+    private int valor;
+    private String mes;
+
+    public Temperatura(int valor, String mes) {
+        this.valor = valor;
+        this.mes = mes;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+
+    public String getMes() {
+        return mes;
+    }
+
+    public void setMes(String mes) {
+        this.mes = mes;
+    }
+
+    @Override
+    public String toString() {
+        return "Temperatura{" +
+                "valor=" + valor +
+                ", mes='" + mes + '\'' +
+                '}';
+    }
 }
